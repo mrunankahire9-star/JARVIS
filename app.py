1aa5c5ef9794418d3c20889b3b3502e109442286
mrunank <mrunank123@gmail.com>
2026-06-21 00:05:27 +0530

    Added login and signup

--- app.py
+++ app.py
@@ -46,3 +46,41 @@ def chatbot():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+    from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("dashboard.html")
+
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        return redirect("/")
+
+    return render_template("login.html")
+
+
+@app.route("/signup", methods=["GET","POST"])
+def signup():
+
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        return redirect("/login")
+
+    return render_template("signup.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
